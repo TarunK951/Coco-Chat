09e158b15f722d376d9a1c429ed5dc110d341c8e
Tarun <119518924+TarunK951@users.noreply.github.com>
2025-05-15 12:44:38 +0530

    nav bar fix

--- coco-chat/src/app/page.js
+++ coco-chat/src/app/page.js
@@ -1,10 +1,8 @@
 import MainPage from "./mainPage/page";
-import NavBar from "./Navbar/page";
 
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <MainPage />
     </div>
   );
