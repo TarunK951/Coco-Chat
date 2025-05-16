e106595813dce16179a761c70054cccc1c628b01
Tarun <119518924+TarunK951@users.noreply.github.com>
2025-05-16 21:15:15 +0530

    coco page creation

---
+++ coco-chat/src/app/coco/page.jsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+function coco() {
+  return (
+    <div>
+      <div>
+        <div className="frosted-glass"></div>
+      </div>
+    </div>
+  );
+}
+
+export default coco;
